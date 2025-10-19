eb5da563120a69b5ab5ca34e831deb8dd76a12e9
HMS 28 <100882136+vlrst@users.noreply.github.com>
2025-10-18 21:56:46 -0400

    Create problem_2.py

---
+++ problem_2.py
@@ -0,0 +1,12 @@
+''' 
+
+You are planning a road trip and want to calculate the fuel needed based on the di tance you will
+travel and your vehicle's fuel efficiency.
+
+'''
+
+
+distance = float(input("Enter distance "))
+fuel = float(input("Enter fuel "))
+
+print(round((distance/fuel), 2))
