cfc56a9a494566e7894c20381c39ac2d51397005
HMS 28 <100882136+vlrst@users.noreply.github.com>
2025-10-24 10:52:28 -0400

    Update problem_6.py

--- problem_6.py
+++ problem_6.py
@@ -14,15 +14,21 @@ for x in range(len(phrase)):
     How to decide whether to put a space or not
     If there is an abbrv and the next one is also an abbrv, put no space
     else: space = False
-    
+
     '''
-    space = False
-    beforeElement = string[0] 
-    if string[0] == string[0].upper():
-        newstr += string[0]
-        
-    if string[0] == string[0].lower():
 
+    beforeElement = string[0]
+    if phrase[x].upper() == "A" or phrase == "I":
+        if beforeElement.upper() == beforeElement.upper():
+            newstr += " "
         newstr += phrase[x]
+        newstr += " "
+    elif string[0] == string[0].upper():
+        newstr += string[0]
 
+    elif string[0] == string[0].lower():
+        newstr += " "
+        newstr += phrase[x]
+        newstr += " "
 
+print(newstr)
