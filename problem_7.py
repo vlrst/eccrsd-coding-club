e05b7f255021fbc7f0cf6915ffeb301b9dd5e457
HMS 28 <100882136+vlrst@users.noreply.github.com>
2025-10-27 11:17:16 -0400

    Update problem_7.py

--- problem_7.py
+++ problem_7.py
@@ -1,4 +1,4 @@
-arr = input("Enter the array ")
+arr = ("4 8 1 3 2")
 arr = arr.split()
 unsort = list(arr)
 
@@ -7,18 +7,14 @@ arr.sort()
 print(arr)
 print(unsort)
 
-
-
 value = 0
 sorted_x = 0
 original_x = 0
-print(unsort.index("3"))
-for x in range(len(arr)):
-    
-    print(f"{arr[x]} has a distance of {unsort.index(arr[x])}")
-    print(f"unsort.index(arr[x]) - {unsort.index(arr[x])} and that is equal to {unsort[(int(arr[x-1]))]}")
-        
-    
-    
-    
-print(value)
+for x in range(len(unsort)):
+    values
+print(unsort.index(str(x))-arr.index(str(x)))
+print(unsort)
+print(arr)
+
+
+
