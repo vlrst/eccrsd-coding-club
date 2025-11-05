8a086bd92bf06af2bcb9cafdc6d709eb4ad127cd
HMS 28 <100882136+vlrst@users.noreply.github.com>
2025-11-05 10:49:17 -0500

    Update problem_7.py

--- problem_7.py
+++ problem_7.py
@@ -1,20 +1,9 @@
-arr = ("4 8 1 3 2")
-arr = arr.split()
-unsort = list(arr)
-
-arr.sort()
-
-print(arr)
-print(unsort)
-
 value = 0
-sorted_x = 0
-original_x = 0
+arr = input().split()
+unsort = list(arr)
+arr.sort()
 for x in range(len(unsort)):
-    values
-print(unsort.index(str(x))-arr.index(str(x)))
-print(unsort)
-print(arr)
-
-
-
+    for y in range(len(arr)):
+        if unsort[x] == arr[y]:
+            value += abs(x-y)
+print(F"This is the value {value}")
